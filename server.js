9fce228308b01127972098e53a2d5b89e67f7ce6
HARIHARAN B P <bphariharan1301@gmail.com>
2024-08-11 22:40:39 +0530

    Update: Server for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const http = require("http");
 
 const app = express();
 const port = 5000;
